761cafedd249266d2d35b1f797a29e1042f3ac07
John <jjpyae@gmail.com>
2023-08-29 15:27:45 -0700

    completes creating HTML element for each grid

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,4 +6,26 @@ let board = new Board(); // creates a new game board
 // Create the UI of the game using HTML elements based on this grid.
 console.log(board.grid);
 
-// Your code here
+document.addEventListener("DOMContentLoaded", () => {
+  const boardContainer = document.createElement('div');
+  boardContainer.setAttribute('id', 'main-board');
+
+  for (let row = 0; row < board.grid.length; row++) {
+    const mainRow = document.createElement('div');
+    mainRow.setAttribute('class', "main-rows");
+    for (let col = 0; col < board.grid[row].length; col++) {
+      createSquare(row, col, mainRow);
+    }
+    boardContainer.appendChild(mainRow);
+  }
+
+  document.body.appendChild(boardContainer);
+
+  function createSquare(row, col, currentMainRow) {
+    const div = document.createElement('div');
+    div.setAttribute('data-col', row);
+    div.setAttribute('data-row', col);
+    div.setAttribute('class', 'each-square');
+    currentMainRow.appendChild(div);
+  }
+});
