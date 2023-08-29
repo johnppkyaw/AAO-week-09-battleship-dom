16aa062d6d399915ff7fbe56afecc831a24dc211
John <jjpyae@gmail.com>
2023-08-29 15:50:07 -0700

    completes phase 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,10 +6,13 @@ let board = new Board(); // creates a new game board
 // Create the UI of the game using HTML elements based on this grid.
 console.log(board.grid);
 
+//creates HTML elements for the grids
 document.addEventListener("DOMContentLoaded", () => {
+  //main board
   const boardContainer = document.createElement('div');
   boardContainer.setAttribute('id', 'main-board');
 
+  //main rows
   for (let row = 0; row < board.grid.length; row++) {
     const mainRow = document.createElement('div');
     mainRow.setAttribute('class', "main-rows");
@@ -19,8 +22,10 @@ document.addEventListener("DOMContentLoaded", () => {
     boardContainer.appendChild(mainRow);
   }
 
+  //append main board to the body
   document.body.appendChild(boardContainer);
 
+  //helper function to create a square for each grid in the main row
   function createSquare(row, col, currentMainRow) {
     const div = document.createElement('div');
     div.setAttribute('data-col', row);
@@ -28,4 +33,16 @@ document.addEventListener("DOMContentLoaded", () => {
     div.setAttribute('class', 'each-square');
     currentMainRow.appendChild(div);
   }
+
+  //Clicking a square
+  boardContainer.addEventListener('click', (event) => {
+    const row = event.target.dataset.row;
+    const col = event.target.dataset.col;
+    if (board.makeHit(row, col) === null) {
+      event.target.classList.add('miss');
+    } else {
+      event.target.classList.add('hit');
+      event.target.textContent = board.grid[row][col]
+    }
+  })
 });
