5528977ec160120d137f502f5e62695117c7cfd6
John <jjpyae@gmail.com>
2023-08-29 17:02:40 -0700

    completes phase 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,42 +7,72 @@ let board = new Board(); // creates a new game board
 console.log(board.grid);
 
 //creates HTML elements for the grids
-document.addEventListener("DOMContentLoaded", () => {
-  //main board
-  const boardContainer = document.createElement('div');
-  boardContainer.setAttribute('id', 'main-board');
+document.addEventListener("DOMContentLoaded", initiateGame);
 
-  //main rows
+//main board
+const boardContainer = document.createElement('div');
+boardContainer.setAttribute('id', 'main-board');
+
+function initiateGame() {
+  //loop thru board grids
   for (let row = 0; row < board.grid.length; row++) {
+
+    //for each row in grid, create a mainrow UI
     const mainRow = document.createElement('div');
     mainRow.setAttribute('class', "main-rows");
+
+    //for each col in the current grid row,
     for (let col = 0; col < board.grid[row].length; col++) {
+      //create a square UI, add that square to mainrow
       createSquare(row, col, mainRow);
     }
+
+    //append each main row UI to main-board
     boardContainer.appendChild(mainRow);
   }
 
   //append main board to the body
   document.body.appendChild(boardContainer);
 
-  //helper function to create a square for each grid in the main row
-  function createSquare(row, col, currentMainRow) {
-    const div = document.createElement('div');
-    div.setAttribute('data-col', row);
-    div.setAttribute('data-row', col);
-    div.setAttribute('class', 'each-square');
-    currentMainRow.appendChild(div);
-  }
-
   //Clicking a square
-  boardContainer.addEventListener('click', (event) => {
-    const row = event.target.dataset.row;
-    const col = event.target.dataset.col;
-    if (board.makeHit(row, col) === null) {
-      event.target.classList.add('miss');
+  boardContainer.addEventListener('click', squareClicker);
+}
+
+//helper function to create a square for each grid in the main row
+function createSquare(row, col, currentMainRow) {
+  const div = document.createElement('div');
+  div.setAttribute('data-col', row);
+  div.setAttribute('data-row', col);
+  div.setAttribute('class', 'each-square');
+  currentMainRow.appendChild(div);
+}
+
+function squareClicker(event) {
+  const targetElement = event.target;
+
+  //Only change the square if it hasn't been clicked
+  if (!targetElement.classList.contains("hit") && !targetElement.classList.contains("miss")) {
+    const row = targetElement.dataset.row;
+    const col = targetElement.dataset.col;
+    const hitOrMiss = board.makeHit(row, col);
+    if (hitOrMiss === null) {
+      targetElement.classList.add('miss');
     } else {
-      event.target.classList.add('hit');
-      event.target.textContent = board.grid[row][col]
+      targetElement.classList.add('hit');
+      targetElement.textContent = board.grid[row][col];
+      const gameOver = board.isGameOver();
+      if (gameOver) {
+        handleGameOver();
+      }
     }
-  })
-});
+  }
+}
+
+//End the game
+function handleGameOver() {
+  const h1 = document.querySelector('h1');
+  const h3 = document.createElement('h3');
+  h3.textContent = "YOU WIN!";
+  h1.insertAdjacentElement("afterend", h3);
+  boardContainer.removeEventListener('click', squareClicker)
+}
